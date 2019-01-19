e7c9438f4c8458d0a4ba8e6c7e5befbb65d02b68
ShayH88 <sanshay88@gmail.com>
2019-01-18 19:15:18 -0600

    Updates

---
+++ precache-manifest.3a6fe3bc51c04ea8c57b4e734a02c995.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ddb82939934e9e935f",
+    "url": "/shayh88/Clicky-Game/static/js/runtime~main.e4ddb829.js"
+  },
+  {
+    "revision": "82d4ae1df8deef8741ca",
+    "url": "/shayh88/Clicky-Game/static/js/main.82d4ae1d.chunk.js"
+  },
+  {
+    "revision": "790a7416686c195da3c6",
+    "url": "/shayh88/Clicky-Game/static/js/1.790a7416.chunk.js"
+  },
+  {
+    "revision": "82d4ae1df8deef8741ca",
+    "url": "/shayh88/Clicky-Game/static/css/main.8b571b46.chunk.css"
+  },
+  {
+    "revision": "c8563d21b878315089d94cfe250f3e60",
+    "url": "/shayh88/Clicky-Game/index.html"
+  }
+];
